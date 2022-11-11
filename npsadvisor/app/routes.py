0be577cfe09e1bbb622b11023d1fc619247e13bf
Shreya Basu <shreyab4@illinois.edu>
2022-11-10 19:49:19 -0600

    Added nav bar back in and updated events.html

--- npsadvisor/app/routes.py
+++ npsadvisor/app/routes.py
@@ -1,9 +1,20 @@
 from flask import Flask, render_template
-# from flask_navigation import Navigation
+from flask_navigation import Navigation
 from app import app
 from app import database as db_helper
 
+nav = Navigation(app)
+
+nav.Bar('top', [
+    nav.Item('Home', 'homepage'),
+    nav.Item('Events', 'events')
+])
+
 @app.route("/")
 def homepage():
-    parks = db_helper.fetch_parks()
+    parks = db_helper.test_fetch()
     return render_template("index.html", parks=parks) # name-db_helper()
+
+@app.route("/events")
+def events():
+    return render_template("events.html")
