d46cd6e2b0eef48390582f174fe046620a1e9c95
fewu2 <fewu2@illinois.edu>
2022-11-11 02:01:52 -0600

    Can't connect front end and backend... why?

--- npsadvisor/app/database.py
+++ npsadvisor/app/database.py
@@ -1,4 +1,6 @@
 from app import db
+import sys
+
 def test_fetch() -> dict:
     parks = [
         {"name": "yellowstone", "description": "maybe exists idk", "stateAbbr": "HI"}
@@ -7,7 +9,7 @@ def test_fetch() -> dict:
 
 def fetch_parks() -> dict:
     conn = db.connect()
-    query_res = conn.execute("SELECT name, description, stateAbbr, directionsUrl FROM Parks LIMIT 10;").fetchall() # Use Python's .format() for user input
+    query_res = conn.execute("SELECT name, description, stateAbbr, directionsUrl FROM Parks ORDER BY name ASC, stateAbbr ASC LIMIT 10;").fetchall() # Use Python's .format() for user input
     conn.close()
     parks = []
     for res in query_res:
@@ -19,3 +21,47 @@ def fetch_parks() -> dict:
         }
         parks.append(item)
     return parks
+
+def search_parks(input):
+    conn = db.connect()
+    input = str(input)
+    print(f"Input2: {input}", file=sys.stderr)
+    query = f"SELECT name, description, stateAbbr, directionsUrl FROM Parks WHERE name LIKE %s ORDER BY name ASC, stateAbbr ASC LIMIT 10;"
+    args = [input+'%']
+    query_res = conn.execute(query, args).fetchall() # Use Python's .format() for user input
+    conn.close()
+    parks = []
+    for res in query_res:
+        item = {
+            "name": res[0],
+            "description": res[1],
+            "states": res[2],
+            "directions": res[3]
+        }
+        parks.append(item)
+    return parks
+
+def get_activities():
+    conn = db.connect()
+    results = conn.execute("Select DISTINCT title from Activities")
+    conn.close()
+    activities = []
+    for result in results:
+        activities.append(result[0])
+    return activities
+
+def get_states():
+    conn = db.connect()
+    results = conn.execute("Select DISTINCT stateAbbr from Parks")
+    conn.close()
+    state = {}
+    states = []
+    for result in results:
+        for i in str(result).split(","):
+            i = i.replace("'", "")
+            i = i.replace("(", "")
+            if state.get(i) == None:
+                state[i] = 1
+                if i != ")":
+                    states.append(i)
+    return states
--- npsadvisor/app/routes.py
+++ npsadvisor/app/routes.py
@@ -1,8 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 from flask_navigation import Navigation
 from app import app
 from app import database as db_helper
-
+import sys
 nav = Navigation(app)
 
 nav.Bar('top', [
@@ -12,9 +12,20 @@ nav.Bar('top', [
 
 @app.route("/")
 def homepage():
-    parks = db_helper.test_fetch()
-    return render_template("index.html", parks=parks) # name-db_helper()
+    activities = db_helper.get_activities()
+    states = db_helper.get_states()
+    parks = db_helper.fetch_parks()
+    return render_template("index.html", activities=activities, states=states, parks=parks) # name-db_helper()
 
 @app.route("/events")
 def events():
     return render_template("events.html")
+
+@app.route("/search", methods = ['GET', 'POST'])
+def search():
+    input = request.form.get('query')
+    print(f"Input: {input}", file=sys.stderr)
+    parks = db_helper.search_parks(input)
+    activities = db_helper.get_activities()
+    states = db_helper.get_states()
+    return render_template("index.html", activities=activities, states=states, parks=parks)
